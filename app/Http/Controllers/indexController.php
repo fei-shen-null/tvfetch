51d42a3ed362d2d696fe0c43cc50fe9c940ca431
Fei <joshua.never@gmail.com>
2016-04-20 00:46:23 +0700

    1

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\TvList;
 use App\User;
+use Cache;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Cookie;
 use Session;
@@ -17,7 +18,9 @@ class IndexController extends Controller
         if (!Session::has('email') && Cookie::has('email')) {
             Session::put('email', Cookie::get('email'));
         }
-        $tvList = TvList::join('tv', 'tv_list.tv_id', '=', 'tv.id')->get()->groupBy('day_of_week');
+        $tvList = Cache::remember('index.tvList', 10, function () {
+            return TvList::join('tv', 'tv_list.tv_id', '=', 'tv.id')->get()->groupBy('day_of_week');
+        });
         $subList = new Collection;
         if (Session::has('email')) {
             $subList = User::byEmail(Session::get('email'))->subTv()->whereIn('tv_id', TvList::all())->pluck('tv_id');
