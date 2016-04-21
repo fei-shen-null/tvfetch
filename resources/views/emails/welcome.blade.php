fd067df241241a123c941a6221cacfc40a3f5ad3
Fei <joshua.never@gmail.com>
2016-04-21 18:01:34 +0700

    1

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,7 +3,7 @@
 <h3>Welcome to TvFetch.</h3>
 <p>Click the checkbox on each TV to sub/unsub.</p>
 <p>You will get notifications when new episodes available.</p>
-<small>This is auto generated mail, please do not replay.Any problem please contact feishennull{at}gmail.com</small>
+<small>This website is still under testing.Any problem please contact fei.shen.null{at}gmail.com</small>
 </body>
 
 </html>
