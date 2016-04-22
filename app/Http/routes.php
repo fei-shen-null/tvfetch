9a6ff72a677b2731a99a5dfbe272c60c7dd34a34
Fei <joshua.never@gmail.com>
2016-04-22 22:15:29 +0700

    1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,13 +6,14 @@ Route::group(['middleware' => 'web'], function () {
     Route::any('unsubscribe/{tv}', 'SubController@unSub');
     Route::any('login', 'SubController@login');
     Route::get('logout', 'SubController@logout');
-    Route::get('cmd/queue/{name}', function ($name) {
-        Artisan::call('queue:work', [
-            '--queue' => $name
-        ]);
-    });
-    Route::get('cmd/getList', function () {
-        Artisan::call('tv:getList');
-    });
+
+//    Route::get('cmd/queue/{name}', function ($name) {
+//        Artisan::call('queue:work', [
+//            '--queue' => $name
+//        ]);
+//    });
+//    Route::get('cmd/getList', function () {
+//        Artisan::call('tv:getList');
+//    });
 });
 
