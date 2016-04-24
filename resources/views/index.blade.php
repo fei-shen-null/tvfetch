b00944498cda7867cd2a9d467516ece679faa400
Fei <joshua.never@gmail.com>
2016-04-24 10:18:23 +0700

    1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,9 @@
             integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
             crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script async src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
+    <script async src="/js/app.js"></script>
 </head>
 <?php  $WEEK = [
         1 => 'Mon',
