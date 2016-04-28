dc4fb4f94eebbdd84ee6adaad7e8a6c1c2792b5b
Fei <joshua.never@gmail.com>
2016-04-28 13:01:08 +0700

    1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() == 'local') {
+            $this->app->register('Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider');
+            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+            $this->app->alias('Debugbar', 'Barryvdh\Debugbar\Facade');
+        }
     }
 }
